b0928ee0b95833e645c754b5bcb252b325cec0ac
miragrio <ionescumat@gmail.com>
2025-12-05 23:05:53 +0000

    Adding dependancies, CORS middleware for frontend

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,8 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
 from tasks.api import router as tasks_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(tasks_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for development you can allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 uvicorn.run(app, host="127.0.0.1", port=8000)
